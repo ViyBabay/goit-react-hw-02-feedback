04a600cbae10795ff1796db70593cbd423be17e1
Oleksandr Pikush <pikush.oleksandr@gmail.com>
2023-01-11 17:42:53 +0200

    v3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,100 @@
-import { Statistics } from "./Statistics/Statistics";
+import React, { Component } from 'react';
+import { Statistics } from './Statistics/Statistics';
+// import { Statistics } from "./Statistics/Statistics";
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      
-      <Statistics />
-    </div>
-  );
-};
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+
+//       <Statistics />
+//     </div>
+//   );
+// };
+
+// import { render } from '@testing-library/react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleGood = () => {
+    this.setState(
+      prevState => ({ good: prevState.good + 1 }),
+      () => console.log(this.state.good)
+    );
+  };
+
+  handleNeutral = () => {
+    this.setState(
+      prevState => ({ neutral: prevState.neutral + 1 }),
+      () => console.log(this.state.neutral)
+    );
+  };
+
+  handleBad = () => {
+    this.setState(
+      prevState => ({ bad: prevState.bad + 1 }),
+      () => console.log(this.state.bad)
+    );
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return ((this.state.good / this.countTotalFeedback()) * 100).toFixed();
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positive = this.countPositiveFeedbackPercentage();
+    return (
+      <div>
+        <h2 className="title">Please leave feedback</h2>
+        <ul className="btn-list">
+          <li className="btn-item">
+            <button type="button" className="btn" onClick={this.handleGood}>
+              Good
+            </button>
+          </li>
+          <li className="btn-item">
+            <button type="button" className="btn" onClick={this.handleNeutral}>
+              Neutral
+            </button>
+          </li>
+          <li className="btn-item">
+            <button type="button" className="btn" onClick={this.handleBad}>
+              Bad
+            </button>
+          </li>
+        </ul>
+        <h3>Statistics</h3>
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positive={positive}
+          />
+        ) : (
+          <h3>There is no feedback</h3>
+        )}
+      </div>
+    );
+  }
+}
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,81 +1,22 @@
-// import { render } from '@testing-library/react';
-import React, { Component } from 'react';
-// import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// let good = 0
+export const Statistics = ({ good, neutral, bad, total, positive }) => {
+  return (
+    <ul>
+      <li className="stat-item">Good: {good}</li>
+      <li className="stat-item">Neutral: {neutral}</li>
+      <li className="stat-item">Bad: {bad}</li>
+      <li className="stat-item">Total: {total}</li>
+      <li className="stat-item">Positive feedback: {positive} %</li>
+    </ul>
+  );
+};
 
-export class Statistics extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  handleGood = () => {
-    this.setState(
-      prevState => ({ good: prevState.good + 1 }),
-      () => console.log(this.state.good)
-    );
-  };
-
-  handleNeutral = () => {
-    this.setState(
-      prevState => ({ neutral: prevState.neutral + 1 }),
-      () => console.log(this.state.neutral)
-    );
-  };
-
-  handleBad = () => {
-    this.setState(
-      prevState => ({ bad: prevState.bad + 1 }),
-      () => console.log(this.state.bad)
-    );
-  };
-
-  countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    return ((this.state.good / this.countTotalFeedback()) * 100).toFixed();
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positive = this.countPositiveFeedbackPercentage();
-    return (
-      <div>
-        <h2 className="title">Please leave feedback</h2>
-        <ul className="btn-list">
-          <li className="btn-item">
-            <button type="button" className="btn" onClick={this.handleGood}>
-              Good
-            </button>
-          </li>
-          <li className="btn-item">
-            <button type="button" className="btn" onClick={this.handleNeutral}>
-              Neutral
-            </button>
-          </li>
-          <li className="btn-item">
-            <button type="button" className="btn" onClick={this.handleBad}>
-              Bad
-            </button>
-          </li>
-        </ul>
-        {total > 0 ? (
-          <ul>
-            <li className="stat-item">Good: {good}</li>
-            <li className="stat-item">Neutral: {neutral}</li>
-            <li className="stat-item">Bad: {bad}</li>
-            <li className="stat-item">Total: {total}</li>
-            <li className="stat-item">Positive feedback: {positive} %</li>
-          </ul>
-        ) : (
-          <h3>There is no feedback</h3>
-        )}
-      </div>
-    );
-  }
-}
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positive: PropTypes.string.isRequired,
+};
